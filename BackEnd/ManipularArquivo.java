82d3fbcddf0489ad92a70dfc22a86468459853d6
Anderson Martins dos Santos <anderson.m.santos@academico.ufs.br>
2019-09-01 20:26:41 -0300

    Classe criada para manipular os arquivos

---
+++ BackEnd/ManipularArquivo.java
@@ -0,0 +1,41 @@
+package BackEnd;
+import java.io.*;
+import java.util.*;
+
+public class ManipularArquivo {
+    File clientesCad = new File("./src/Arquivos/clientesCadastrados.txt");
+    private String cadastrados = null;
+    public void escreverArquivo(String objeto) throws IOException{
+        FileWriter cadastrarFile = new FileWriter(clientesCad, true);
+        PrintWriter cadastrarPrint = new PrintWriter(cadastrarFile);
+        cadastrarPrint.println(objeto);
+        cadastrarPrint.flush();
+        cadastrarPrint.close();
+        cadastrarFile.close();
+    }
+    public void lerArquivo(File arquivo) throws IOException{
+        FileReader listarFile = new FileReader(arquivo);
+        BufferedReader listarBuff = new BufferedReader(listarFile);
+        while (listarBuff.ready()) {
+            String linhaL = listarBuff.readLine();
+            cadastrados += linhaL + "\n";
+        }
+        listarBuff.close();
+        listarFile.close();
+    }
+    public String getCadastrados(){
+        return cadastrados;
+    }
+    public void mapearArquivo(File arquivo) throws IOException{
+        BufferedReader mapearBuff = new BufferedReader(new InputStreamReader(new FileInputStream(arquivo)));
+        String linhaM = null;
+        int numLinha = 0;
+        HashMap<Integer, String> linhasArquivo = new HashMap<Integer, String>();
+        while((linhaM = mapearBuff.readLine()) != null){
+            linhasArquivo.put(numLinha, linhaM);
+            numLinha++;
+        }
+        mapearBuff.close();
+        //usar linhasArquivo.get(NUMERO DA LINHA);
+    }
+}
