68cf227c12afc453df83614bd0c0f1336e262809
Alexandro <alexandro.silva@dcomp.ufs.br>
2019-09-11 16:15:01 -0300

    ATUALIZADO

---
+++ src/BackEnd/ProdutoInterface.java
@@ -0,0 +1,17 @@
+
+package BackEnd;
+
+public interface ProdutoInterface {
+  public void setCodigo();
+  public int getCodigo();
+  public void setNome();
+  public String getNome();
+  public void setPrecoVenda();
+  public int getPrecoVenda();
+  public void setPrecoCusto();
+  public int getPrecoCusto();   
+  public void setQuantidade();
+  public int getQuantidade();
+  public void setFornecedor();
+  public String getFornecedor();
+}
